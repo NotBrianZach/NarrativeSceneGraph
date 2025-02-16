70f2042c2c25890dcbb0774874ebe4c146042489
Erik K. <androydgba@gmail.com>
2025-02-16 12:48:35 -0300

    progress

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 run: .venv
 	source .venv/bin/activate &&\
-	./main.py
+	./main.py $(ARGS)
 .venv: # should only run once
-	chmod +x main.py
+	curl https://openrouter.ai/api/v1/models | python -m json.tool > .models
 	python -m venv .venv
 	source .venv/bin/activate &&\
 	pip install -r requirements.txt
+
+.PHONY: run
--- main.py
+++ main.py
@@ -3,38 +3,42 @@ if __name__ != "__main__":
     raise Exception("program is not a module; execute it directly")
 
 
+import json.tool
 import sys
 import os
 from pathlib import Path
+import json
+import requests
 
 try:
-    API_TOKEN = Path('.API_TOKEN').read_text()
+    API_TOKEN =  os.environ.get(".API_TOKEN", Path('.API_TOKEN').read_text())
 except FileNotFoundError as e:
-    raise FileNotFoundError("paste your OpenRouter token into `.API_TOKEN`") from e
+    raise FileNotFoundError("provide the OpenRouter token through:\n- an environment variable `.API_TOKEN`\n- a file `.API_TOKEN` containing it") from e
 USAGE_STR = f"usage: {sys.argv[0]} <filename> <model>"
+OUT_DIR = Path("out")
 FORMATS = ('pdf',) # TODO: moar
-MODELS = ('openai/gpt-4o',) # TODO: moar; these will be the model strings
-OUT_FOLDER = Path("out")
+MODELS = Path('.models')
 
 try:
     fname: str = sys.argv[1]
+    # TODO: bash completion or choose from index, will be easier with a TUI
     model: str = sys.argv[2]
 except IndexError as e:
     raise IndexError(USAGE_STR) from e
 fextension = fname[fname.rfind('.')+1:]
 if fextension not in FORMATS:
     raise ValueError("unavailable file format; choose from " + str(FORMATS))
+if model not in MODELS:
+    raise ValueError("unavailable model; choose from " + str(MODELS))
 
 # pylint: disable=wrong-import-position
 import time
-import logging
-# these imports take so long so I placed them after initial checks
+# these imports take long so I placed them after initial checks
 # even if it disrespects pylint C0413
 from marker.converters.pdf import PdfConverter
-from marker.modelws import create_model_dict
+from marker.models import create_model_dict
 from marker.output import text_from_rendered
-# pylint: enableq=wrong-import-position
-logging.getLogger().setLevel(level=3)
+# pylint: enable=wrong-import-position
 
 FORMAT_CONVERTERS = {'pdf': PdfConverter}
 assert tuple(FORMAT_CONVERTERS.keys()) == FORMATS
@@ -47,4 +51,30 @@ rendered = converter(fname)
 text, _, images = text_from_rendered(rendered)
 print(f"took:\t{round(time.time() - tstart, 2)}s (imports + parsing)")
 
-# TODO: split into nodes
+# get info about limits: https://openrouter.ai/docs/api-reference/limits#rate-limits-and-credits-remaining
+res = requests.get(
+    url="https://openrouter.ai/api/v1/auth/key",
+    headers={
+        "Authorization": "Bearer "+API_TOKEN,
+    },
+    timeout=10,
+)
+res.raise_for_status()
+print(f"OpenRouter tokens remaining: {res.json()['data']['limit_remaining']}")
+
+res = requests.post(
+  url="https://openrouter.ai/api/v1/chat/completions",
+  headers={
+    "Authorization": "Bearer"+API_TOKEN,
+  },
+  data=json.dumps({
+    "model": model,
+    "messages": [text]
+  }),
+  timeout=30,
+).json()
+
+if not res.ok:
+    # https://openrouter.ai/docs/api-reference/errors
+    print(res.error.message, file=sys.stderr)
+    res.raise_for_status()
