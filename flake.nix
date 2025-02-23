535c39877a9e3a58c780a094710411d630d52a96
Zach Abel <poinkcompany@gmail.com>
2025-02-23 09:41:44 -0600

    add zach reproducibility tooling wip

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Development environment for NarrativeSceneGraph";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.python311        # Use the desired Python version
+          pkgs.gcc              # Provides libstdc++
+           pkgs.python311Packages.numpy        # NumPy package for Python 3.11
+        ];
+
+        # Set LD_LIBRARY_PATH to include the gcc libraries
+        # shellHook = ''
+        #   export LD_LIBRARY_PATH="${pkgs.gcc.libc}/lib:$LD_LIBRARY_PATH"
+        #   echo "LD_LIBRARY_PATH set to: $LD_LIBRARY_PATH"
+        # '';
+      };
+    };
+}
+
